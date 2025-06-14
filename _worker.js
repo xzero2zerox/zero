51d1b5b7ec32a250cd730f065c12810c7e268d2b
xzero2zerox <x.zero2zero.x@gmail.com>
2025-06-14 12:41:53 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,8 +3,8 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "duckdns.org"; // Ganti dengan domain utama kalian
-const serviceName = "zero2zero"; // Ganti dengan nama workers kalian
+const rootDomain = "zero2zero.duckdns.org"; // Ganti dengan domain utama kalian
+const serviceName = "zero"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
