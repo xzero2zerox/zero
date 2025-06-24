ca01452c2a66e76049f002c09fa92b51463f4fd8
xzero2zerox <x.zero2zero.x@gmail.com>
2025-06-24 15:41:21 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "zero2zero.workers.dev"; // Ganti dengan domain utama kalian
+const rootDomain = "z2z.workers.dev"; // Ganti dengan domain utama kalian
 const serviceName = "zero"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
